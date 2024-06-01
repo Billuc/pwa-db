fe6b028d2bad35b2714f94430905228991ede576
Billuc <billuc@hotmail.fr>
2024-06-01 08:21:11 +0200

    feat(store): Added missing methods to get and update data

--- src/store.ts
+++ src/store.ts
@@ -10,16 +10,90 @@ export default class Store {
   }
 
   async get(key: IDBValidKey): Promise<any> {
-    const results = await this.openTransaction(this._storeName, "readonly", (t) => new Promise((res, rej) => {
+    const results = await this.openTransaction(this._storeName, "readonly", (t) => new Promise<any>((res, rej) => {
       const store = t.getStore();
       const request = store.get(key);
-      request.onsuccess = () => {
-        res(request.result);
-      };
-      request.onerror = () => {
-        rej("Couldn't get data from the store")
-      }
+
+      request.onsuccess = () => res(request.result);
+      request.onerror = () => rej(request.error);
     }));
+    return results;
+  }
+
+  async getAll(range?: IDBKeyRange): Promise<any[]> {
+    const results = await this.openTransaction(this._storeName, "readonly", (t) => new Promise<any[]>((res, rej) => {
+      const store = t.getStore();
+      const request = store.getAll(range)
+
+      request.onsuccess = () => res(request.result);
+      request.onerror = () => rej(request.error);
+    }))
+    return results;
+  }
+
+  async getFromIndex(indexName: string, key: IDBValidKey): Promise<any> {
+    const results = await this.openTransaction(this._storeName, "readonly", (t) => new Promise<any>((res, rej) => {
+      const store = t.getStore();
+      const index = store.index(indexName);
+      const request = index.get(key);
+
+      request.onsuccess = () => res(request.result);
+      request.onerror = () => rej(request.error);
+    }));
+    return results;
+  }
+
+  async getAllFromIndex(indexName: string, range?: IDBKeyRange): Promise<any[]> {
+    const results = await this.openTransaction(this._storeName, "readonly", (t) => new Promise<any[]>((res, rej) => {
+      const store = t.getStore();
+      const index = store.index(indexName);
+      const request = index.getAll(range);
+
+      request.onsuccess = () => res(request.result);
+      request.onerror = () => rej(request.error);
+    }))
+    return results;
+  }
+
+  async add(value: any): Promise<IDBValidKey> {
+    const results = await this.openTransaction(this._storeName, "readwrite", (t) => new Promise<IDBValidKey>((res, rej) => {
+      const store = t.getStore();
+      const request = store.add(value);
+
+      request.onsuccess = () => res(request.result);
+      request.onerror = () => rej(request.error);
+    }))
+    return results;
+  }
+
+  async delete(key: IDBValidKey): Promise<void> {
+    await this.openTransaction(this._storeName, "readwrite", (t) => new Promise<void>((res, rej) => {
+      const store = t.getStore();
+      const request = store.delete(key);
+
+      request.onsuccess = () => res();
+      request.onerror = () => rej(request.error);
+    }))
+  }
+
+  async deleteAll(range?: IDBKeyRange): Promise<void> {
+    await this.openTransaction(this._storeName, "readwrite", (t) => new Promise<void>((res, rej) => {
+      const store = t.getStore();
+      const request = !!range ? store.delete(range) : store.clear();
+
+      request.onsuccess = () => res();
+      request.onerror = () => rej(request.error);
+    }))
+  }
+
+  async put(value: any): Promise<IDBValidKey> {
+    const results = await this.openTransaction(this._storeName, "readwrite", (t) => new Promise<IDBValidKey>((res, rej) => {
+      const store = t.getStore();
+      const request = store.put(value);
+
+      request.onsuccess = () => res(request.result);
+      request.onerror = () => rej(request.error)
+    }))
     return results;
   }
 
